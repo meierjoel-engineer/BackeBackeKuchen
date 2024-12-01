289aec3a94d3a9329604229e7a51e3d445b88a89
meierjoel-engineer <meierjoel.extern@gmail.com>
2024-12-01 20:05:07 +0100

    works without deepsleep form espc3

--- Server/Server.py
+++ Server/Server.py
@@ -2,7 +2,7 @@ import socket
 
 # Server IP and Port
 HOST = '192.168.241.218'  # Listen on all available interfaces
-PORT = 65432      # Port to listen on
+PORT = 65432              # Port to listen on
 
 def start_server():
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -10,25 +10,26 @@ def start_server():
     s.listen(1)  # Allow 1 connection at a time
     print(f"Server listening on {HOST}:{PORT}")
 
-    conn, addr = s.accept()
-    print(f"Connection from {addr}")
+    while True:
+        conn, addr = s.accept()
 
-    try:
-        while True:
-            data = conn.recv(1024)  # Buffer size is 1024 bytes
-            if not data:
-                break
-            print(f"Received: {data.decode('utf-8')}")
-    except Exception as e:
-        print(f"Error: {e}")
-    finally:
-        conn.close()
-        s.close()
-        print("Server closed")
+        try:
+            with open('received_data.txt', 'a') as file:
+                while True:
+                    data = conn.recv(1024)  # Buffer size is 1024 bytes
+                    if not data:
+                        break
+                    decoded_data = data.decode('utf-8')
+                    print(f"Received: {decoded_data}")
+                    file.write(decoded_data + '\n')
+        except Exception as e:
+            print(f"Error: {e}")
+        finally:
+            conn.close()
+
 
 if __name__ == '__main__':
     start_server()
-
 #esptool --chip esp32c3 --port COM12 erase_flash
 #esptool --chip esp32c3 --port COM12 --baud 460800 write_flash -z 0x0 C:\GitRepos\BackeBackeKuchen\bin\ESP32_GENERIC_C3-20241025-v1.24.0.bin
 #ampy --port COM12 put C:\GitRepos\BackeBackeKuchen\ESP\main.py
